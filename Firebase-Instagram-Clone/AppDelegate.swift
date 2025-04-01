93f7a42bcc2005d8776fcf4ba6f419c7016786da
PunhanShahmur <punhan.sahmurov.1616@gmail.com>
2025-04-01 19:07:36 +0400

    added Firebase SDK

--- Firebase-Instagram-Clone/AppDelegate.swift
+++ Firebase-Instagram-Clone/AppDelegate.swift
@@ -6,6 +6,12 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseAnalytics
+import FirebaseFirestore
+import FirebaseStorage
+import FirebaseCore
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +20,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
+        
         return true
     }
 
