34b220e8d1ec35a39469e24a55a5ea5db1fc44fb
PunhanShahmur <punhan.sahmurov.1616@gmail.com>
2025-04-03 19:08:01 +0400

    added Settins Controller

---
+++ Firebase-Instagram-Clone/SettingsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SettingsViewController.swift
+//  Firebase-Instagram-Clone
+//
+//  Created by Punhan Shahmurov on 01.04.25.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func logOut(_ sender: Any) {
+        
+        performSegue(withIdentifier: "toLoginVC", sender: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Firebase-Instagram-Clone/ViewController.swift
+++ Firebase-Instagram-Clone/ViewController.swift
@@ -6,14 +6,82 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailLabel: UITextField!
+    
+    @IBOutlet weak var passwordLabel: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
     }
 
-
+    @IBAction func signUp(_ sender: Any) {
+        
+        if emailLabel.text != "" && passwordLabel.text != "" {
+            
+            Auth.auth().createUser(withEmail: emailLabel.text!, password: passwordLabel.text!) { (result, error) in
+            
+                if error != nil {
+                    
+                    self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
+                    
+                } else {
+                    
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                    
+                }
+                
+            }
+            
+        } else{
+          
+            makeAlert(title: "Error", message: "Email or password is empty")
+            
+        }
+        
+        
+        
+        
+    }
+    
+    @IBAction func signIn(_ sender: Any) {
+        
+        if emailLabel.text != "" && passwordLabel.text != "" {
+            
+            Auth.auth().signIn(withEmail: emailLabel.text!, password: passwordLabel.text!) { (result, error) in
+                
+                if error != nil {
+                    
+                    self.makeAlert(title: "Error", message: error?.localizedDescription ?? "Error")
+                    
+                } else {
+                    
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                    
+                }
+                
+            }
+            
+            
+        } else{
+            makeAlert(title: "Error", message: "Email or password is empty")
+        }
+        
+    }
+    
+    func makeAlert(title: String, message: String) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+          let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
+          alert.addAction(okButton)
+          self.present(alert, animated: true, completion: nil)
+    }
+    
 }
 
